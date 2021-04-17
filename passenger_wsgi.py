f2e4160286a70ad140019fdc8677672f115046ad
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2021-04-17 14:05:04 +0100

    checking in passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,32 @@
 import os
 import sys
 
+import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.dirname(__file__))
+# Set up paths and environment variables
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
+# Set script name for the PATH_INFO fix below
+SCRIPT_NAME = os.getcwd()
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+class PassengerPathInfoFix(object):
+    """
+        Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        from urllib.parse import unquote
+        environ['SCRIPT_NAME'] = SCRIPT_NAME
+        request_uri = unquote(environ['REQUEST_URI'])
+        script_name = unquote(environ.get('SCRIPT_NAME', ''))
+        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+        return self.app(environ, start_response)
+
+# Set the application
+application = get_wsgi_application()
+application = PassengerPathInfoFix(application)
